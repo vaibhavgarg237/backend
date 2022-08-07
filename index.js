c61fa6334f9aa494c727ef92c2c5539d1e0be921
Vaibhav Garg <vaibhav.garg237@gmail.com>
2022-08-07 15:20:37 +0530

    express server basic

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const connect2Mongo = require("./db");
+const express = require("express");
+const app = express();
+const port = 3000;
 
 connect2Mongo();
+
+app.get("/", (req, res) => {
+	res.send("Hello Vaibhav!");
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
