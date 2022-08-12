237e4fd4432e676cc7be2f16aa221d178a5f0369
Vaibhav Garg <vaibhav.garg237@gmail.com>
2022-08-12 21:41:40 +0530

    cors policy

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const connect2Mongo = require("./db");
 const express = require("express");
 const app = express();
 const port = 5000;
+const cors = require("cors");
 
 connect2Mongo();
 
+app.use(cors());
 app.use(express.json());
 
 //Available routes
@@ -12,5 +14,5 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`iNotes-backend app listening on port ${port}`);
 });
