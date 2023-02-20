9d4b6e6fde81031f8ef2be20de11fd28e22c50a3
vaibhav garg <vaibhav.garg237@gmail.com>
2023-02-20 21:59:49 +0530

    fix: port + home-endp point

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const connect2Mongo = require("./db");
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 connect2Mongo();
 
+app.get("/", (req, res) => {
+  console.log("Home end point hit");
+  res.send("Testing my server");
+});
 app.use(cors());
 app.use(express.json());
 
@@ -14,5 +18,5 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-	console.log(`iNotes-backend app listening on port ${port}`);
+  console.log(`iNotes-backend app listening on port ${port}`);
 });
--- routes/notes.js
+++ routes/notes.js
@@ -6,100 +6,100 @@ const { body, validationResult } = require("express-validator");
 
 //Route 1: Get all the notes: GET "/api/notes/fetchallnotes". Login required
 router.get("/fetchallnotes", fetchusers, async (req, res) => {
-	const notes = await Notes.find({ user: req.user.id });
-	res.json(notes);
+  const notes = await Notes.find({ user: req.user.id });
+  res.json(notes);
 });
 
 //Route 2: Add a new note: POST "/api/notes/addnote". Login required
 router.post(
-	"/addnote",
-	fetchusers,
-	[
-		body("title", "Enter a valid title").isLength({ min: 3 }),
-		body("description", "Enter valid description").isLength({ min: 5 }),
-	],
-	async (req, res) => {
-		//if(errors) return bad request
-		const errors = validationResult(req); //written above
-		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
-		}
-		try {
-			//title,desc,tag are destructured and stored separately to create a new note obj
-			const { title, description, tag } = req.body;
-			const note = new Notes({
-				title,
-				description,
-				tag,
-				user: req.user.id,
-			});
-			const savedNote = await note.save();
-			res.json(savedNote);
-		} catch (error) {
-			console.log(error.message);
-			res.status(500).send("Internal server error");
-		}
-	}
+  "/addnote",
+  fetchusers,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Enter valid description").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    //if(errors) return bad request
+    const errors = validationResult(req); //written above
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      //title,desc,tag are destructured and stored separately to create a new note obj
+      const { title, description, tag } = req.body;
+      const note = new Notes({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
 );
 
 //Route 4: Update note: PUT "/api/notes/updatenote". Login required
 router.put("/updatenote/:id", fetchusers, async (req, res) => {
-	try {
-		//title,desc,tag are destructured and stored separately to create a new note obj
-		const { title, description, tag, id } = req.body;
-		const newNote = {};
-		if (title) {
-			newNote.title = title;
-		}
-		if (description) {
-			newNote.description = description;
-		}
-		if (tag) {
-			newNote.tag = tag;
-		}
-		//fetch note from db for updation
-		//what if note doesn't exists
-		let note = await Notes.findById(req.params.id);
-		if (!note) {
-			return res.status(404).send("Not found");
-		}
-		//what if note user and login user are different!
-		if (note.user.toString() !== req.user.id) {
-			return res.status(401).send("Not allowed!");
-		}
-		//update
-		note = await Notes.findByIdAndUpdate(
-			req.params.id,
-			{ $set: newNote },
-			{ new: true } //this makes, any new stuff create instead of creation
-		);
-		res.json({ note });
-	} catch (error) {
-		console.log(error.message);
-		res.status(500).send("Internal server error");
-	}
+  try {
+    //title,desc,tag are destructured and stored separately to create a new note obj
+    const { title, description, tag, id } = req.body;
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+    //fetch note from db for updation
+    //what if note doesn't exists
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not found");
+    }
+    //what if note user and login user are different!
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not allowed!");
+    }
+    //update
+    note = await Notes.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true } //this makes, any new stuff create instead of creation
+    );
+    res.json({ note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+  }
 });
 
 //Route 5: Delete note: Delete "/api/notes/deletenote". Login required
 router.delete("/deletenote/:id", fetchusers, async (req, res) => {
-	try {
-		//fetch note from db for deletion
-		//what if note doesn't exists
-		let note = await Notes.findById(req.params.id);
-		if (!note) {
-			return res.status(404).send("Not found");
-		}
-		//what if note user and login user are different!
-		if (note.user.toString() !== req.user.id) {
-			return res.status(401).send("Not allowed!");
-		}
-		//delete
-		note = await Notes.findByIdAndDelete(req.params.id);
-		res.json({ Success: "Note has been deleted", note: note });
-	} catch (error) {
-		console.log(error.message);
-		res.status(500).send("Internal server error");
-	}
+  try {
+    //fetch note from db for deletion
+    //what if note doesn't exists
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not found");
+    }
+    //what if note user and login user are different!
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not allowed!");
+    }
+    //delete
+    note = await Notes.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note has been deleted", note: note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+  }
 });
 
 module.exports = router;
